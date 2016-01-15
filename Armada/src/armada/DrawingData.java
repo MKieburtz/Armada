3cf552c08e9d5a5fb21ee03550aa8be2956ac984
codeNinja77 <hightechninja7@gmail.com>
2016-01-14 19:38:27 -0800

    Added some stuff for drawing

--- Armada/src/armada/DrawingData.java
+++ Armada/src/armada/DrawingData.java
@@ -1,9 +1,29 @@
 package armada;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 /**
  * @author Michael Kieburtz
  */
 public class DrawingData 
 {
-    
+    ArrayList<Ship> ships = new ArrayList<>();
+    BufferedImage sideBorder;
+    BufferedImage topBorder;
+
+    public void setShips(ArrayList<Ship> ships) 
+    {
+        this.ships = ships;
+    }
+
+    public void setSideBorder(BufferedImage sideBorder) 
+    {
+        this.sideBorder = sideBorder;
+    }
+
+    public void setTopBorder(BufferedImage topBorder) 
+    {
+        this.topBorder = topBorder;
+    }   
 }
